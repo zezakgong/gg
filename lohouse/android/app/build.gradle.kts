0325207677dc653ec1009d6e8861c7369b56570a
zezakgong <zezakgong@163.com>
2026-09-08 20:13:34 +0800

    提高版本号测试

--- lohouse/android/app/build.gradle.kts
+++ lohouse/android/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 1
-        versionName = "1.0.1"
+        versionCode = 2
+        versionName = "1.0.2"
     }
 
     buildTypes {
